05bb74718452a1235974ce98b50e11d16bd76283
CANguru-System <gustavwostrack@web.de>
2023-07-29 10:14:57 +0200

    Updated 65-Install

--- 65-CANguru-Install-Software/CANguru-Install.cmd
+++ 65-CANguru-Install-Software/CANguru-Install.cmd
@@ -15,8 +15,9 @@ echo.
 echo  1 - COM-Port festlegen
 echo  2 - Flash-Speicher loeschen
 echo  3 - OTA-Upload vorbereiten auf %COMPORT%
-echo  4 - Upload (aus Ordner CANguru-Files) ueber %COMPORT%
-echo  5 - Putty starten
+echo  4 - Upload CANguru-Decoder (aus Ordner CANguru-Files) ueber %COMPORT%
+echo  5 - Upload CANguru-Bridge (aus Ordner CANguru-Bridge) ueber %COMPORT%
+echo  6 - Putty starten
 echo. 
 echo  x - Beenden
 echo.
@@ -27,68 +28,23 @@ if "%SELECTED%" == "1" goto :SetComPort
 if "%SELECTED%" == "2" goto :ERASE_FLASH
 if "%SELECTED%" == "3" goto :PREPARE_OTA
 if "%SELECTED%" == "4" goto :UPLOAD_FIRMWARE
-if "%SELECTED%" == "5" goto :Putty
+if "%SELECTED%" == "5" goto :UPLOAD_BRIDGE
+if "%SELECTED%" == "6" goto :Putty
 
 goto :errorInput 
 
 
 :SetComPort
-echo  1 - Setze COM-Port 1
-echo  2 - Setze COM-Port 2
-echo  3 - Setze COM-Port 3
-echo  4 - Setze COM-Port 4
-echo  5 - Setze COM-Port 5
-echo  6 - Setze COM-Port 6
-echo  7 - Setze COM-Port 7
-echo  8 - Setze COM-Port 8
-echo  9 - Setze COM-Port 9
-echo  10 - Setze COM-Port 10
-echo  x - Exit
+REM @echo OFF
+REM FOR /L %%x IN (1, 1, 29) DO ECHO %%x - Setze COM-Port %%x
+echo Bitte geben Sie die Nummer des COM-Anschlusses ein (z.B. 5 fuer COM5) oder x fuer Exit
 echo.
 set /p SELECTED=Ihre Auswahl: 
 
 if "%SELECTED%" == "x" goto :loop
- 
-if "%SELECTED%" == "1" (
-set COMPORT=COM1
+
+set COMPORT=COM%SELECTED%
 goto :loop
-)
-if "%SELECTED%" == "2" (
-set COMPORT=COM2
-goto :loop
-)
-if "%SELECTED%" == "3" (
-set COMPORT=COM3
-goto :loop
-)
-if "%SELECTED%" == "4" (
-set COMPORT=COM4
-goto :loop
-)
-if "%SELECTED%" == "5" (
-set COMPORT=COM5
-goto :loop
-)
-if "%SELECTED%" == "6" (
-set COMPORT=COM6
-goto :loop
-)
-if "%SELECTED%" == "7" (
-set COMPORT=COM7
-goto :loop
-)
-if "%SELECTED%" == "8" (
-set COMPORT=COM8
-goto :loop
-)
-if "%SELECTED%" == "9" (
-set COMPORT=COM9
-goto :loop
-)
-if "%SELECTED%" == "10" (
-set COMPORT=COM10
-goto :loop
-)
 goto :errorInput 
 
 pause
@@ -106,6 +62,14 @@ goto :loop
 @echo on
 esptool.exe --chip esp32 --port %COMPORT% --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 Prepare-OTA/bootloader.bin 0x8000 Prepare-OTA/partitions.bin 0xe000 Prepare-OTA/boot_app0.bin 0x10000 Prepare-OTA/firmware.bin
 Putty\putty.exe -serial %COMPORT% -sercfg 115200,8,n,1,N
+@echo off
+echo.
+pause
+goto :loop
+
+:UPLOAD_BRIDGE
+@echo on
+esptool.exe --chip esp32 --port %COMPORT% --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 CANguru-Bridge/bootloader.bin 0x8000 CANguru-Bridge/partitions.bin 0x10000 CANguru-Bridge/firmware.bin
 @echo off
 echo.
 pause
