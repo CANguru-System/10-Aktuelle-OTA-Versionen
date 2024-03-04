c597a1c16d349c6796e3f11a9c1dab4f3a078e08
CANguru-System <gustavwostrack@web.de>
2024-03-04 12:29:40 +0100

    Add files via upload

---
+++ 000-CANguru-Files-Version-3.0/01-CANguru-Bridge-Olimex- Version 3.03 large LCD/README.md
@@ -0,0 +1,3 @@
+# 70-CANguruBridge-V5.00
+Version 5.00 der CANguru-Bridge
+!!!!!!!!!!! Experimental and uncomplete !!!!!!!!!!!!!!!!!
---
+++ 000-CANguru-Files-Version-3.0/01-CANguru-Bridge-Olimex- Version 3.03 large LCD/lib/src/CANguru.h
@@ -0,0 +1,18 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <CANguru-Buch@web.de> wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return
+ * Gustav Wostrack
+ * ----------------------------------------------------------------------------
+ */
+#ifndef CANGURU_H
+#define CANGURU_H
+
+// Bmp-Logo of an CANguru
+#include <stdint.h>
+
+const String CgVersionnmbr = "3.15";
+const String CgVersion = "CANguru " + CgVersionnmbr;
+
+#endif // CANGURU_H
